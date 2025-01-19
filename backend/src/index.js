9f284781362c9de2709acaed69ec680ca5c930fc
Kishioki <filbertysn14@gmail.com>
2025-01-19 18:15:58 +0700

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,4 +9,3 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use('/', routes);
 app.listen(CONFIG.PORT, () => console.log(`Server is running on port ${CONFIG.PORT}`));
-//
