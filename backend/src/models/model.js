a18ce2f2caa5bf6aae2d0f82704b2b29c04b99a2
Kishioki <filbertysn14@gmail.com>
2025-01-21 23:55:16 +0700

    CI/CD

--- backend/src/models/model.js
+++ backend/src/models/model.js
@@ -1,8 +1,17 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
-const db = new Sequelize('ecommerce', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
+// Load environment variables
+const DB_NAME = process.env.DB_NAME || 'ecommerce';
+const DB_USER = process.env.DB_USER || 'root';
+const DB_PASSWORD = process.env.DB_PASSWORD || '';
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_DIALECT = process.env.DB_DIALECT || 'mysql';
+
+// Set up Sequelize
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  logging: false, // Disable Sequelize logging
 });
 
 export { db, DataTypes };
